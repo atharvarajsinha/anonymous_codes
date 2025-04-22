5cb90b2198d424a6a4fd547e41b3676ebed82b4d
atharvarajsinha <sinhaatharva@gmail.com>
2025-04-23 01:53:50 +0530

    CRUD APIs of Environments

--- backend/mainapp/urls.py
+++ backend/mainapp/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import signup, login
+from .views import addEnv, getEnvs, getSpecificEnv,updateEnv, delEnv
 
 urlpatterns = [
     path('signup/', signup),
     path('login/', login),
+
+    path('envs/add/', addEnv),
+    path('envs/', getEnvs),
+    path('envs/specific/', getSpecificEnv),
+    path('envs/update/<int:id>/', updateEnv),
+    path('envs/delete/<int:id>/', delEnv),
 ]
--- backend/mainapp/views.py
+++ backend/mainapp/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.contrib.auth import authenticate
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .models import Environment
+from .serializers import EnvironmentSerializer
 
 # Create your views here.
 @api_view(["POST"])
@@ -45,3 +48,58 @@ def login(request):
         }, status=status.HTTP_200_OK)
 
     return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+# ENVIRONMENT APIS
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def addEnv(request):
+    serializer = EnvironmentSerializer(data=request.data)
+    if serializer.is_valid():
+        env = serializer.save()
+        return Response({"message":"Added Successfully"}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def getEnvs(request):
+    envs = Environment.objects.all()
+    serializer = EnvironmentSerializer(envs, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def getSpecificEnv(request):
+    var_name = request.GET.get("variable_name")
+    try:
+        env = Environment.objects.get(variable_name=var_name)
+        serializer = EnvironmentSerializer(env)
+        return Response(serializer.data)
+    except Environment.DoesNotExist:
+        return Response({"error": "Environment not found"}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(["PUT"])
+@permission_classes([IsAuthenticated])
+def updateEnv(request, id):
+    try:
+        env = Environment.objects.get(id=id)
+    except Environment.DoesNotExist:
+        return Response({"error": "Environment not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = EnvironmentSerializer(env, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            "message":"Updated Successfully",
+            "data":serializer.data
+        })
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["DELETE"])
+@permission_classes([IsAuthenticated])
+def delEnv(request, id):
+    try:
+        env = Environment.objects.get(id=id)
+        env.delete()
+        return Response({"message": "Environment deleted"})
+    except Environment.DoesNotExist:
+        return Response({"error": "Environment not found"}, status=status.HTTP_404_NOT_FOUND)
