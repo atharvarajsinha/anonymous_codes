8442a122d9b140508937100644471aeac28e7afe
atharvarajsinha <sinhaatharva@gmail.com>
2025-04-23 04:23:53 +0530

    cors-headers

--- backend/api_nexus/settings.py
+++ backend/api_nexus/settings.py
@@ -27,6 +27,11 @@ SECRET_KEY = 'django-insecure-hx@9)*@s*e9pa=p^nq&ap-p8eage8s5^f0kn_(#no_tb)y9*^_
 DEBUG = True
 
 ALLOWED_HOSTS = []
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+]
 
 
 # Application definition
@@ -45,10 +50,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'mainapp',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
