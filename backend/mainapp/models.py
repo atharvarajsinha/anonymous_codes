335f7034a718c8d3ba2b15a47876fea0b98de4f9
atharvarajsinha <sinhaatharva@gmail.com>
2025-04-23 02:07:29 +0530

    requests table

--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -7,3 +7,12 @@ class Environment(models.Model):
 
     def __str__(self):
         return self.variable_name
+    
+class Request(models.Model):
+    request_method = models.CharField(max_length=10)
+    request_name = models.CharField(max_length=255)
+    request_url = models.URLField()
+    is_collected = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.request_name
